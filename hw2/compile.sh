9c156f74040a1d75b17a57442ca862d109e2963d
Yuxi Liu <yuxi.liu.1995@gmail.com>
2022-09-25 09:59:00 -0700

    data collected and analyzed

--- hw2/compile.sh
+++ hw2/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 file=cs285/report/reported_data.txt
 rm -rf reported_data
 mkdir -p reported_data
@@ -10,9 +8,9 @@ while read line; do
     cp -r "data/${line}" reported_data; 
 done < "${file}"
 
-# mkdir -p submit
-# rm -rf submit/cs285
-# rm -rf submit/run_logs
-# rm -f submit.zip
-# cp -r cs285 submit/cs285
-# cp -r data submit/run_logs
+mkdir -p submit
+rm -rf submit/cs285
+rm -rf submit/run_logs
+rm -f submit/submit.zip
+cp -r cs285 submit/cs285
+cp -r reported_data submit/run_logs
