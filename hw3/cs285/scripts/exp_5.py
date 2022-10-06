8fb6aae92de0c3d666d6e70864f7f32c7b8b68a4
Yuxi Liu <33951560+yuxiliu1995@users.noreply.github.com>
2022-10-06 14:04:37 -0700

    Use halfcheetah-v4

--- hw3/cs285/scripts/exp_5.py
+++ hw3/cs285/scripts/exp_5.py
@@ -5,7 +5,7 @@ ngsptu = 10 # number of gradient steps per critic network update
 
 commands = []
 commands.append("python cs285/scripts/run_hw3_actor_critic.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
-commands.append("python cs285/scripts/run_hw3_actor_critic.py --env_name HalfCheetah-v2 --ep_len 150 -- discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
+commands.append("python cs285/scripts/run_hw3_actor_critic.py --env_name HalfCheetah-v4 --ep_len 150 -- discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
 
 if __name__ == "__main__":
     for command in commands:
