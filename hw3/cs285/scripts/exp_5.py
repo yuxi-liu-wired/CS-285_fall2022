af8ed46ca719fb5fde00a71bd6c5071561d862bc
Yuxi Liu <yuxi.liu.1995@gmail.com>
2022-10-06 22:05:58 -0700

    half cheetah

--- hw3/cs285/scripts/exp_5.py
+++ hw3/cs285/scripts/exp_5.py
@@ -6,7 +6,7 @@ ngsptu = 10 # number of gradient steps per critic network update
 command_stem = "python cs285/scripts/run_hw3_actor_critic.py --env_name "
 commands = []
 commands.append(command_stem+"InvertedPendulum-v4 --ep_len 1000 --discount 0.95 -n 100 -l 2 -s 64 -b 5000 -lr 0.01 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
-commands.append(command_stem+"HalfCheetah-v2 --ep_len 150 --discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
+commands.append(command_stem+"HalfCheetah-v4 --ep_len 150 --discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 -b 30000 -eb 1500 -lr 0.02 --exp_name q5_{ntu}_{ngsptu} -ntu {ntu} -ngsptu {ngsptu}".format(ntu=ntu, ngsptu=ngsptu))
 
 if __name__ == "__main__":
     for command in commands:
